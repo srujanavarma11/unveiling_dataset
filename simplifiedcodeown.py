c66cc54c11b6cb598a5c8b2d9c5a9fcff19444de
srujanavarma11 <147147627+srujanavarma11@users.noreply.github.com>
2023-10-10 12:26:36 +0530

    ML_Code.py

--- simplifiedcodeown.py
+++ simplifiedcodeown.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 def method():
-    df = pd.read_csv('https://raw.githubusercontent.com/BhuvanGampala/HackathonData/main/data.csv')
+    df = pd.read_csv('https://raw.githubusercontent.com/srujanavarma11/unveiling_dataset/main/data.csv')
     y = df['unit_consumption']
     X = df.drop(['_id','Time_Stamp','Device_ID','_updated','_created','_etag','Real_Power','unit_consumption'], axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
